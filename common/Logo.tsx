1778a0ce46a2d8cae5f1ecc63e155a38bd9a3989
Ferhat Mohamed Tahar <ferhattahar46@gmail.com>
2025-11-01 18:46:18 +0100

    FIX:fixed logo dark theme

--- common/Logo.tsx
+++ common/Logo.tsx
@@ -1,14 +1,15 @@
 "use client";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
-// import { Link }
+import LogoPicDark from "../public/glados-dark.png";
+import LogoPic from "../public/glados.svg";
 function Logo() {
+  const { theme } = useTheme();
+  const logoSrc = theme === "light" ? LogoPic : LogoPicDark;
   return (
     <Link href="/" className="flex items-center justify-center gap-2 group">
-      {/* <span className=" text-2xl font-bold text-[var(--primary)] ">
-        Glados Zone
-      </span> */}
-      <Image src="/glados.svg" alt="Glados Zone Logo" width={30} height={30} />
+      <Image src={logoSrc} alt="Glados Zone Logo" width={30} height={30} />
       <span className=" text-2xl font-bold text-[var(--primary)] hidden sm:inline-block group-hover:text-[var(--foreground)] transition-colors duration-300">
         Glados Zone
       </span>
@@ -17,27 +18,3 @@ function Logo() {
 }
 
 export default Logo;
-// "use client";
-// import Image from "next/image";
-// import Link from "next/link";
-// import LogoPic from "../public/glados.svg";
-// import LogoPicDark from "../public/glados.svg";
-// import { useTheme } from "next-themes";
-// // import { Link }
-// function Logo() {
-//   const { theme } = useTheme();
-//   const logoSrc = theme === "light" ? Logo : LogoPicDark;
-//   return (
-//     <Link href="/" className="flex items-center justify-center gap-2 group">
-//       {/* <span className=" text-2xl font-bold text-[var(--primary)] ">
-//         Glados Zone
-//       </span> */}
-//       <Image src="/glados.svg" alt="Glados Zone Logo" width={30} height={30} />
-//       <span className=" text-2xl font-bold text-[var(--primary)] hidden sm:inline-block group-hover:text-[var(--foreground)] transition-colors duration-300">
-//         Glados Zone
-//       </span>
-//     </Link>
-//   );
-// }
-
-// export default Logo;
